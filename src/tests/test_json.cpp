3137cdc3719a3122456db13c9dabc4dfd69b7bfe
Manuel MINANA <efficiel.technologie@gmail.com>
2016-01-31 19:30:24 +0100

    test json ok

--- src/tests/test_json.cpp
+++ src/tests/test_json.cpp
@@ -1,7 +1,37 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include "json/json.h"
 
-int main(int argc, char ** argv) {
+#define TEST_INPUT_FILE "src/tests/data/server.json"
 
-    return 1;
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    
+    /* sanity */
+    {
+        ifstream ifs(TEST_INPUT_FILE);
+        if (ifs.fail()) {
+            cout << "fail to open " << TEST_INPUT_FILE << endl;
+            return 1;
+        } else {
+            ifs.close();
+        }
+    }
+
+    Json::Value root;
+    Json::Reader reader;
+    std::ifstream test(TEST_INPUT_FILE, ifstream::binary);
+
+    bool parsingSuccessful = reader.parse(test, root, false);
+
+    assert( parsingSuccessful && "json parse data file");
+
+    assert(root["host"].asString() == string("localhost"));
+    assert(root["port"].asString() == string("9990"));
+
+
+    return 0;
 }
